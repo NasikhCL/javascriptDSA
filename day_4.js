159df66ac3eff7781ff3b7a1ad544ca168b8de0d
Nasikh CL <nasikcl@gmail.com>
2024-01-21 11:58:54 +0530

    new function to check palandrom

--- day_4.js
+++ day_4.js
@@ -1,32 +1,40 @@
 // palindrome problem
 
 function checkPalindrome(str){
-    if(!str){
-        return false;
+    let string1 = str, revString1 =''
+    for(let i =string1.length-1; i>=0;i--){
+        revString1 += string1[i]
     }
-    str = str.toLowerCase();
-    let TransStr="";
-    for(i=0; i <str.length;i++){
-        if(str[i] === " "){
-
-        }
-        else{
-            TransStr += str[i]
-        }
-        console.log(TransStr)
-    }
-    let revStr = ""
-    for(let i=TransStr.length-1; i>=0; i--){
-        revStr += TransStr[i];
-        
-    }
-    if(TransStr === revStr){
-        return true;
-    }
-    else{
-        return false
-    }
-    // console.log(revStr)
+    return string1 === revString1
 }
 
-console.log(checkPalindrome('hello olleh'))
+// function checkPalindrome(str){
+//     if(!str){
+//         return false;
+//     }
+//     str = str.toLowerCase();
+//     let TransStr="";
+//     for(i=0; i <str.length;i++){
+//         if(str[i] === " "){
+
+//         }
+//         else{
+//             TransStr += str[i]
+//         }
+//         console.log(TransStr)
+//     }
+//     let revStr = ""
+//     for(let i=TransStr.length-1; i>=0; i--){
+//         revStr += TransStr[i];
+        
+//     }
+//     if(TransStr === revStr){
+//         return true;
+//     }
+//     else{
+//         return false
+//     }
+//     // console.log(revStr)
+// }
+
+console.log(checkPalindrome('malay alam'))
